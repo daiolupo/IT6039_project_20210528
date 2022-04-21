ee4d337e687bea797e82bea15296ce6c62b38b68
Dwain Aiolupotea <d.aiolupo@gmail.com>
2022-04-21 20:39:22 +1200

    In GutterGame change to assertEqual and check 10 instead of 0. testcase passed but failed.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import unittest
 import BowlingGame
+
 
 class TestBowlingGame(unittest.TestCase):
 
@@ -12,30 +12,36 @@ class TestBowlingGame(unittest.TestCase):
 
     def testGutterGame(self):
         for i in range(0, 20):
-            self.game.rolls(0)
-        assert self.game.score()==0
+            self.game.roll(0)
+        self.assertEqual(self.game.score(), 10)
+
     def testAllOnes(self):
         self.rollMany(1, 20)
-        assert self.game.score()==20
+        assert self.game.score() == 20
+
     def testOneSpare(self):
         self.game.rolls(5)
         self.game.rolls(5)
         self.game.rolls(3)
-        self.rollMany(0,17)
-        assert self.game.score()==16
+        self.rollMany(0, 17)
+        assert self.game.score() == 16
+
     def testOneStrike(self):
         self.game.rolls(10)
         self.game.rolls(4)
         self.game.rolls(3)
-        self.rollMany(0,16)
-        assert  self.game.score()==24
+        self.rollMany(0, 16)
+        assert self.game.score() == 24
+
     def testPerfectGame(self):
-        self.rollMany(10,12)
-        assert self.game.score()==300
+        self.rollMany(10, 12)
+        assert self.game.score() == 300
+
     def testOneSpare(self):
-        self.rollMany(5,21)
-        assert self.game.score()==150
-    def rollMany(self, pins,rolls):
+        self.rollMany(5, 21)
+        assert self.game.score() == 150
+
+    def rollMany(self, pins, rolls):
         for i in range(rolls):
             self.game.rolls(pins)
 
